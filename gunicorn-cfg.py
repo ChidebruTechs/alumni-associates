2513acfc166c67502fc632e94ad2776a80038a89
Chidebru Techs <chidebrutechs@gmail.com>
2022-12-06 11:02:13 +0300

    revert back to app.py

---
+++ gunicorn-cfg.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2020 - present Chidebru Techs
+"""
+
+bind = '0.0.0.0:5005'
+workers = 1
+accesslog = '-'
+loglevel = 'debug'
+capture_output = True
+enable_stdio_inheritance = True
